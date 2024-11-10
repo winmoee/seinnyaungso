6f767f1d5c4fd5bd139ac40bdce604b2c309875d
Win Moe <winmoe@berkeley.edu>
2024-11-10 01:05:13 -0800

    1 hour 16

---
+++ ecommerceadmin/pages/api/products.js
@@ -0,0 +1,16 @@
+import clientPromise from "@/lib/db";
+import mongoose from "mongoose";
+import {Product} from "@/models/Product"
+import { mongooseConnect } from "@/lib/mongoose";
+
+export default async function handle(req, res) {
+    const {method} = req;
+    await mongooseConnect();
+    if (method === 'POST') {
+        const {title, description, price} = req.body;
+        const productDoc = await Product.create({
+            title, description, price,
+        })
+        res.json(productDoc);
+    }
+}
--- ecommerceadmin/pages/products/new.js
+++ ecommerceadmin/pages/products/new.js
@@ -1,9 +1,34 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
+import {useState} from "react";
 
 export default function NewProduct() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+
+    async function createProduct(ev) {
+        ev.preventDefault();
+        const data = {title, description, price}
+        await axios.post('/api/products', data)
+    }
     return (
         <Layout>
-            <input type="text" placeholder="product name"/>
+            <form onSubmit={createProduct}>
+
+                <h1>New Product</h1>
+                <label>Product Name</label>
+                <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
+
+                <label>Description </label>
+                <textarea placeholder="description" value={description} onChange={ev => setDescription(ev.target.value)}></textarea>
+
+                <label>Price (MMK)</label>
+                <input type="number" placeholder="price" value={price} onChange={ev => setPrice(ev.target.value)}></input>
+
+                <button className="btn-primary">Save</button>
+
+            </form>
         </Layout>
     )
 }
