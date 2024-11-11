c325c47faa3531c44557ed779927a812ffbd0701
Win Moe <winmoe@berkeley.edu>
2024-11-11 02:38:30 -0800

    useState for edit form works

---
+++ ecommerceadmin/components/ProductForm.js
@@ -0,0 +1,36 @@
+import Layout from "@/components/Layout";
+import axios from "axios";
+import { useRouter } from "next/router";
+import {useState} from "react";
+
+export default function ProductForm({title:existingTitle, description:existingDescription, price:existingPrice}) {
+    const [title, setTitle] = useState(existingTitle || '');
+    const [description, setDescription] = useState(existingDescription || '');
+    const [price, setPrice] = useState(existingPrice || '');
+    const [goToProducts, setGoToProducts] = useState(false);
+    const router = useRouter();
+    async function createProduct(ev) {
+        ev.preventDefault();
+        const data = {title, description, price}
+        await axios.post('/api/products', data)
+        setGoToProducts(true);
+    }
+    if (goToProducts) {
+        router.push('/products');
+    }
+    return (
+            <form onSubmit={createProduct}>
+                <label>Product Name</label>
+                <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
+
+                <label>Description </label>
+                <textarea placeholder="description" value={description} onChange={ev => setDescription(ev.target.value)}></textarea>
+
+                <label>Price (MMK)</label>
+                <input type="number" placeholder="price" value={price} onChange={ev => setPrice(ev.target.value)}></input>
+
+                <button className="btn-primary">Save</button>
+
+            </form>
+    )
+}
--- ecommerceadmin/pages/products/new.js
+++ ecommerceadmin/pages/products/new.js
@@ -1,34 +1,11 @@
 import Layout from "@/components/Layout";
-import axios from "axios";
-import {useState} from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function NewProduct() {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-
-    async function createProduct(ev) {
-        ev.preventDefault();
-        const data = {title, description, price}
-        await axios.post('/api/products', data)
-    }
     return (
-        <Layout>
-            <form onSubmit={createProduct}>
-
-                <h1>New Product</h1>
-                <label>Product Name</label>
-                <input type="text" placeholder="product name" value={title} onChange={ev => setTitle(ev.target.value)}/>
-
-                <label>Description </label>
-                <textarea placeholder="description" value={description} onChange={ev => setDescription(ev.target.value)}></textarea>
-
-                <label>Price (MMK)</label>
-                <input type="number" placeholder="price" value={price} onChange={ev => setPrice(ev.target.value)}></input>
-
-                <button className="btn-primary">Save</button>
-
-            </form>
-        </Layout>
+    <Layout>
+        <h1>New Product</h1>
+        <ProductForm />
+    </Layout>
     )
 }
